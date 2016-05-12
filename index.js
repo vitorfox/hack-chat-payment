8910190a24250f4e0140267e52b4fe2bb0821fdc
Vitor Teixeira <vitorfox@gmail.com>
2016-05-12 18:40:46 -0300

    Add css and nickname

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/css/*', function(req, res){
+  res.sendFile(__dirname + req.path);
+});
+
+app.get('/users', function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(users));
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('set nickname', function(nickname) {
+    console.log('nickname received', nickname);
+    users.push(nickname);
+    console.log(users);
+  });
+  socket.on('disconnect', function () {
+    //socket.emit('disconnected');
+    console.log('disconnected. should remove user');
   });
 });
 
